67bf3b736ef5a1e92faa281e5416a3461d389f3b
shimi <50517268+kirscheBlatt@users.noreply.github.com>
2019-09-16 18:28:52 +0900

    commit
    commit

---
+++ Practice/app/src/main/java/com/example/a81903/practice/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.a81903.practice;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.a81903.practice.fragment.BlankFragment;
+import com.example.a81903.practice.fragment.Tab1Fragment;
+import com.example.a81903.practice.fragment.Tab2Fragment;
+import com.example.a81903.practice.fragment.Tab4Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("タブ1"));
+        tabLayout.addTab(tabLayout.newTab().setText("タブ２"));
+        tabLayout.addTab(tabLayout.newTab().setText("タブ３"));
+
+        ViewPager viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+
+            switch (position){
+                case 0:
+                    fragment = new Tab1Fragment();
+                    break;
+                case 1:
+                    fragment = new Tab2Fragment();
+                    break;
+                case 2:
+                    fragment = new BlankFragment();
+                    break;
+                case 3:
+                    fragment = new Tab4Fragment();
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public String getPageTitle(int position){
+            String tabTitle = null;
+
+            switch (position){
+                case 0:
+                    tabTitle = "おもてなし";
+                    break;
+                case 1:
+                    tabTitle = "旬（８月）";
+                    break;
+                case 2:
+                    tabTitle = "新着順";
+                    break;
+                case 3:
+                    tabTitle = "人気";
+                    break;
+            }
+
+            return tabTitle;
+        }
+
+    }
+
+
+}
---
+++ Practice/app/src/main/java/com/example/a81903/practice/fragment/Tab1Fragment.java
@@ -0,0 +1,27 @@
+package com.example.a81903.practice.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a81903.practice.R;
+
+/**
+ * Tab1
+ */
+public class Tab1Fragment extends Fragment {
+
+    public Tab1Fragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+}
