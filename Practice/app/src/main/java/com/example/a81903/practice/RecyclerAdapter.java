ff0cd0fb29c727a0e1cb17c28a193b3fd0e1bceb
shimi <50517268+kirscheBlatt@users.noreply.github.com>
2019-09-18 00:44:17 +0900

    no message

--- Practice/app/src/main/java/com/example/a81903/practice/RecyclerAdapter.java
+++ Practice/app/src/main/java/com/example/a81903/practice/RecyclerAdapter.java
@@ -1,4 +1,66 @@
 package com.example.a81903.practice;
 
-public class RecyclerAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private LayoutInflater mInflater;
+    private ArrayList<String> mData;
+    private Context mContext;
+
+    public RecyclerAdapter(Context context,ArrayList<String> data){
+        mInflater = LayoutInflater.from(context);
+        mContext = context;
+        mData = data;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View inflate = mInflater.inflate(R.layout.cell_1,viewGroup,false);
+       ViewHolder viewHolder = new ViewHolder(inflate);
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+        if (mData != null && mData.size() > i && mData.get(i) != null) {
+            ViewHolder holder = (ViewHolder)viewHolder;
+            holder.textView.setText(mData.get(i));
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if (mData != null){
+            return mData.size();
+        }else {
+            return 0;
+        }
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+         TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+
+
 }
+
--- Practice/app/src/main/java/com/example/a81903/practice/fragment/Tab2Fragment.java
+++ Practice/app/src/main/java/com/example/a81903/practice/fragment/Tab2Fragment.java
@@ -1,13 +1,20 @@
 package com.example.a81903.practice.fragment;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.a81903.practice.R;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,12 +26,46 @@ public class Tab2Fragment extends Fragment {
         // Required empty public constructor
     }
 
+    private Activity mActivity =null;
+    private View mView;
+    private Tab2FragmentListener mFragmentListener = null;
+
+    private RecyclerView mRecyclerView = null;
+    private RecyclerView.Adapter mAdapter =null;
+
+    public interface Tab2FragmentListener{
+        void onRecyclerEvent();
+    }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        mView = inflater.inflate(R.layout.fragment_tab2,container,false);
+
+        mRecyclerView =  mView.findViewById(R.id.recycler_view);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+
+        return mView;
+
+
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // 適当にデータ作成
+        ArrayList<String> array = new ArrayList<>();
+        array.add("A");
+        array.add("B");
+        array.add("C");
+
+
+    }
 }
+
