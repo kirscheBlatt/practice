53265772a7bdfbbffde91075d02a579d69e06369
shimi <50517268+kirscheBlatt@users.noreply.github.com>
2019-09-25 23:48:54 +0900

    no message

--- Practice/app/src/main/java/com/example/a81903/practice/ProcessSync.java
+++ Practice/app/src/main/java/com/example/a81903/practice/ProcessSync.java
@@ -1,6 +1,22 @@
 package com.example.a81903.practice;
 
+import java.util.logging.LogManager;
+
 public final class ProcessSync {
     private static final long TIME_SPAN_BLOCKING_MILLISECONDS = 200L;
+    private static String TAG = "ProcessSync";
+    private static ProcessSync s_Instance = null;
 
+    private long mTimestamp = 0;
+
+    private ProcessSync(){
+    }
+
+    private static ProcessSync getInstance(){
+        return s_Instance;
+    }
+
+    private static void preparationInstance(){
+       
+    }
 }
