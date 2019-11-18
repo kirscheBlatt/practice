4e716ab79425d79adf98d148d0c80c401325b019
shimi <50517268+kirscheBlatt@users.noreply.github.com>
2019-11-18 22:34:04 +0900

    簡単なボックス

--- Practice/app/src/main/java/com/example/a81903/practice/LoginActivity.java
+++ Practice/app/src/main/java/com/example/a81903/practice/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +21,14 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        final CheckBox checkBox = findViewById(R.id.checkbox1);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                
             }
         });
 
--- Practice/app/src/main/java/com/example/a81903/practice/fragment/Tab2Fragment.java
+++ Practice/app/src/main/java/com/example/a81903/practice/fragment/Tab2Fragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import com.example.a81903.practice.R;
 import com.example.a81903.practice.RecyclerAdapter;
