5f17042b1d5dba65f1b8c6d85eff1ae37f05bd1b
shimi <50517268+kirscheBlatt@users.noreply.github.com>
2019-12-09 22:19:52 +0900

    fragment

--- Practice2/app/src/main/java/com/example/practice2/MainActivity.java
+++ Practice2/app/src/main/java/com/example/practice2/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.practice2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
