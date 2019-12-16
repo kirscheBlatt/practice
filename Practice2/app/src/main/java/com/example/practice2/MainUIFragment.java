aa28a095fe9699efdc6e2c0aa67c271912d5fd85
shimi <50517268+kirscheBlatt@users.noreply.github.com>
2019-12-16 22:07:59 +0900

    no message

--- Practice2/app/src/main/java/com/example/practice2/MainUIFragment.java
+++ Practice2/app/src/main/java/com/example/practice2/MainUIFragment.java
@@ -5,16 +5,21 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Locale;
 
 
 public class MainUIFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private String[] dataset = new String[20];
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -25,25 +30,35 @@ public class MainUIFragment extends Fragment {
     public MainUIFragment() {
     }
 
-    public static MainUIFragment newInstance(String param1, String param2) {
-        MainUIFragment fragment = new MainUIFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_ui, container, false);
+        View v = inflater.inflate(R.layout.fragment_main_ui,container,false);
+
+        RecyclerView recyclerView = (RecyclerView)v.findViewById(R.id.userRecyclerView);
+
+
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager rLayoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(rLayoutManager);
+
+        for(int i=0; i < dataset.length; i++) {
+            dataset[i] = String.format(Locale.ENGLISH, "Data_0%d", i);
+        }
+
+
+        RecyclerView.Adapter rAdapter = new MyAdapter(dataset);
+        recyclerView.setAdapter(rAdapter);
+
+        return v;
     }
 
 
--- Practice2/app/src/main/java/com/example/practice2/MyAdapter.java
+++ Practice2/app/src/main/java/com/example/practice2/MyAdapter.java
@@ -1,4 +1,48 @@
 package com.example.practice2;
 
-public class MyAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] dataset = new String[20];
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mTextView;
+
+        ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView)v.findViewById(R.id.text_view);
+        }
+    }
+
+    MyAdapter(String[] myDataset) {
+        dataset = myDataset;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.dummy_user_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.mTextView.setText(dataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.length;
+    }
+
 }
